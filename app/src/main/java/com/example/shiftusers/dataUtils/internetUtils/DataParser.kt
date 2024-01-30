50373ca81aa679aa314c36b2d834ec42023177ee
mkKurish <kurish04@mail.ru>
2024-01-30 23:36:22 +0300

    randomuser.me access

--- app/src/main/java/com/example/shiftusers/dataUtils/internetUtils/DataParser.kt
+++ app/src/main/java/com/example/shiftusers/dataUtils/internetUtils/DataParser.kt
@@ -1,2 +1,40 @@
 package com.example.shiftusers.dataUtils.internetUtils
 
+import android.content.Context
+import com.example.shiftusers.structures.ShiftUser
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+
+object DataParser{
+    fun saveUsersData(){
+        TODO("Implementation with realm")
+    }
+
+    fun getOrGenerateUsers(appContext: Context): List<ShiftUser> {
+        TODO("Try with getStoredUsers(), else generateUsers()")
+    }
+
+    fun generateUsers(appContext: Context, count: Int = 10): List<ShiftUser> {
+        val generatedUsers = mutableListOf<ShiftUser>()
+        repeat(count){
+            getUser(appContext)?.let { it1 -> generatedUsers.add(it1) }
+        }
+        return generatedUsers
+    }
+
+    private fun getStoredUsers(): List<ShiftUser> {
+        TODO("Implementation with realm")
+    }
+
+    private fun getUser(appContext: Context): ShiftUser? {
+        var user: ShiftUser?
+        runBlocking {
+            user = async {
+                val rawJSON = DataReceiver.getRawUserData(appContext)
+                rawJSON?.let { Json.decodeFromString<ShiftUser>(it) }
+            }.await()
+        }
+        return user
+    }
+}
--- app/src/main/java/com/example/shiftusers/dataUtils/internetUtils/DataReceiver.kt
+++ app/src/main/java/com/example/shiftusers/dataUtils/internetUtils/DataReceiver.kt
@@ -1,2 +1,22 @@
 package com.example.shiftusers.dataUtils.internetUtils
 
+import android.content.Context
+import android.widget.Toast
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.request
+
+object DataReceiver {
+    private const val RANDOM_USERS_API_URI = "https://randomuser.me/api/"
+
+    suspend fun getRawUserData(appContext: Context): String? {
+        if (checkInternet(appContext)) {
+            val client = HttpClient(CIO)
+            val response = client.request<String>(RANDOM_USERS_API_URI)
+            println(response)
+            return response
+        } else Toast.makeText(appContext, "Проблема с доступом в Интернет", Toast.LENGTH_SHORT)
+            .show()
+        return null
+    }
+}
